dd5a2775f54fe123ea6966b8820265976c88738f
SlimTim10 <slimtim10@gmail.com>
2019-06-25 13:17:43 -0400

    Port static example to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  getWeather = event => {
+    event.preventDefault();
+    
+    const apiKey = '';
+    const rootEndpoint = `https://api.openweathermap.org/data/2.5/weather?APPID=${apiKey}`;
+
+    const city = event.target.elements.city.value;
+
+    const endpoint = rootEndpoint
+          + `&units=metric`
+          + `&q=${city}`;
+    console.log('Endpoint:', endpoint);
+
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+        console.log(`Current weather: ${data.weather[0].description}`);
+        console.log(`Current temperature: ${data.main.temp}`);
+      });
+  }
+  
+  render() {
+    return (
+      <div>
+        <aside id="ui-control">
+          <h2>Enter a city name</h2>
+          <form onSubmit={this.getWeather}>
+            <input name="city" type="text" />
+            <button id="get-weather">Get weather</button>
+          </form>
+        </aside>
+
+        <main>
+          <article>The weather is: <span id="current-weather"></span></article>
+          <article>The temperature is: <span id="current-temperature"></span></article>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
